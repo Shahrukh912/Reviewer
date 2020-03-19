ad2e63c5e069e1826313c64f87a3eb285b9bdbda
Shahrukh Siddiqui <44868127+Shahrukh912@users.noreply.github.com>
2020-03-19 13:45:48 +0530

    Add files via upload

---
+++ masterheader.php
@@ -0,0 +1,18 @@
+<div class="HeaderMain">
+	<div class="webname">
+		<p>Reviewer</p>
+	</div>
+	<div class="search">
+		<form>
+			<input type="search" name="search" placeholder="Search for Website or Profile">
+			<input type="submit" name="searchsubmit" value="Search" >
+		</form>
+	</div>
+	<div class="navbuttons">
+		<form action="<?php $_PHP_SELF?>" method="get">
+		    <button type="submit" formmethod="post">Profile</button>
+			<button >About us</button>
+			<button >Logout</button>
+		</form>
+	</div>
+</div>
